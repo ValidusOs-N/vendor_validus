769892df5f7151400a1649153eb7704906a57f98
theringmaster <gzroms@hotmail.com>
2016-09-06 14:02:53 -0400

    Changes for Validus
    Credits to Dirty Unicorns for the initial vendor repo

--- config/common.mk
+++ config/common.mk
@@ -9,8 +9,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 endif
 
 PRODUCT_COPY_FILES += \
-    vendor/du/prebuilt/bin/sysinit:system/bin/sysinit \
-    vendor/du/prebuilt/etc/init.du.rc:root/init.du.rc
+    vendor/validus/prebuilt/bin/sysinit:system/bin/sysinit \
+    vendor/validus/prebuilt/etc/init.validus.rc:root/init.validus.rc
 
 PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
@@ -28,25 +28,25 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Backup Tool
 PRODUCT_COPY_FILES += \
-    vendor/du/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/du/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/du/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
+    vendor/validus/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/validus/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
+    vendor/validus/prebuilt/common/bin/50-base.sh:system/addon.d/50-base.sh \
 
 # Backup Services whitelist
 PRODUCT_COPY_FILES += \
-    vendor/du/config/permissions/backup.xml:system/etc/sysconfig/backup.xml
+    vendor/validus/config/permissions/backup.xml:system/etc/sysconfig/backup.xml
 
 # Init file
 PRODUCT_COPY_FILES += \
-    vendor/du/prebuilt/common/etc/init.local.rc:root/init.du.rc
+    vendor/validus/prebuilt/common/etc/init.local.rc:root/init.du.rc
 
 # Copy latinime for gesture typing
 PRODUCT_COPY_FILES += \
-    vendor/du/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
+    vendor/validus/prebuilt/common/lib/libjni_latinimegoogle.so:system/lib/libjni_latinimegoogle.so
 
 # SELinux filesystem labels
 PRODUCT_COPY_FILES += \
-    vendor/du/prebuilt/common/etc/init.d/50selinuxrelabel:system/etc/init.d/50selinuxrelabel
+    vendor/validus/prebuilt/common/etc/init.d/50selinuxrelabel:system/etc/init.d/50selinuxrelabel
 
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
@@ -54,12 +54,12 @@ PRODUCT_COPY_FILES += \
 
 # Don't export PS1 in /system/etc/mkshrc.
 PRODUCT_COPY_FILES += \
-    vendor/du/prebuilt/common/etc/mkshrc:system/etc/mkshrc \
+    vendor/validus/prebuilt/common/etc/mkshrc:system/etc/mkshrc \
 
 PRODUCT_COPY_FILES += \
-    vendor/du/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
-    vendor/du/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit \
-    vendor/du/prebuilt/common/bin/sysinit:system/bin/sysinit
+    vendor/validus/prebuilt/common/etc/init.d/00banner:system/etc/init.d/00banner \
+    vendor/validus/prebuilt/common/etc/init.d/90userinit:system/etc/init.d/90userinit \
+    vendor/validus/prebuilt/common/bin/sysinit:system/bin/sysinit
 
 # Stagefright FFMPEG plugin
 ifneq ($(BOARD_USES_QCOM_HARDWARE),true)
@@ -74,42 +74,75 @@ PRODUCT_PROPERTY_OVERRIDES += \
 endif
 
 # Packages
-include vendor/du/config/packages.mk
+include vendor/validus/config/packages.mk
 
-PRODUCT_PACKAGE_OVERLAYS += vendor/du/overlay/common
+PRODUCT_PACKAGE_OVERLAYS += vendor/validus/overlay/common
 
-# Boot Animation
+# Boot animation include
+ifneq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
+
+# determine the smaller dimension
+TARGET_BOOTANIMATION_SIZE := $(shell \
+  if [ $(TARGET_SCREEN_WIDTH) -lt $(TARGET_SCREEN_HEIGHT) ]; then \
+    echo $(TARGET_SCREEN_WIDTH); \
+  else \
+    echo $(TARGET_SCREEN_HEIGHT); \
+  fi )
+
+# get a sorted list of the sizes
+bootanimation_sizes := $(subst .zip,, $(shell ls vendor/validus/prebuilt/common/bootanimation))
+bootanimation_sizes := $(shell echo -e $(subst $(space),'\n',$(bootanimation_sizes)) | sort -rn)
+
+# find the appropriate size and set
+define check_and_set_bootanimation
+$(eval TARGET_BOOTANIMATION_NAME := $(shell \
+  if [ -z "$(TARGET_BOOTANIMATION_NAME)" ]; then
+    if [ $(1) -le $(TARGET_BOOTANIMATION_SIZE) ]; then \
+      echo $(1); \
+      exit 0; \
+    fi;
+  fi;
+  echo $(TARGET_BOOTANIMATION_NAME); ))
+endef
+$(foreach size,$(bootanimation_sizes), $(call check_and_set_bootanimation,$(size)))
+
+ifeq ($(TARGET_BOOTANIMATION_HALF_RES),true)
 PRODUCT_COPY_FILES += \
-    vendor/du/prebuilt/common/media/bootanimation.zip:system/media/bootanimation.zip
+    vendor/validus/prebuilt/common/bootanimation/halfres/$(TARGET_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
+else
+PRODUCT_COPY_FILES += \
+    vendor/validus/prebuilt/common/bootanimation/$(TARGET_BOOTANIMATION_NAME).zip:system/media/bootanimation.zip
+endif
+endif
 
 # SuperSU
 PRODUCT_COPY_FILES += \
-    vendor/du/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
-    vendor/du/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
+    vendor/validus/prebuilt/common/UPDATE-SuperSU.zip:system/addon.d/UPDATE-SuperSU.zip \
+    vendor/validus/prebuilt/common/etc/init.d/99SuperSUDaemon:system/etc/init.d/99SuperSUDaemon
 
 # Versioning System
 ANDROID_VERSION = 7.0
-DU_VERSION = v11.0
-ifndef DU_BUILD_TYPE
-    DU_BUILD_TYPE := DIRTY-DEEDS
-    PLATFORM_VERSION_CODENAME := DIRTY-DEEDS
+VALIDUS_VERSION = v10.0
+ifndef VALIDUS_BUILD_TYPE
+    VALIDUS_BUILD_TYPE := WOLVESBAYNE
+    PLATFORM_VERSION_CODENAME := WOLVESBAYNE
 endif
 
-#Build DU-Updater only if DU_BUILD_TYPE isn't DIRTY-DEEDS
-ifneq ($(DU_BUILD_TYPE),DIRTY-DEEDS)
-PRODUCT_PACKAGES += \
-    DU-Updater
-endif
+#Build VALIDUS-Updater only if VALIDUS_BUILD_TYPE isn't WOLVESBAYNE
+#ifneq ($(VALIDUS_BUILD_TYPE),WOLVESBAYNE)
+#PRODUCT_PACKAGES += \
+#    VALIDUS-Updater
+#endif
 
 # easy way to extend to add more packages
 -include vendor/extra/product.mk
 
 # Set all versions
-DU_VERSION := DU_$(DU_BUILD)_$(ANDROID_VERSION)_$(shell date -u +%Y%m%d-%H%M).$(DU_VERSION)-$(DU_BUILD_TYPE)
-DU_MOD_VERSION := DU_$(DU_BUILD)_$(ANDROID_VERSION)_$(shell date -u +%Y%m%d-%H%M).$(DU_VERSION)-$(DU_BUILD_TYPE)
+VALIDUS_VERSION := VALIDUS_$(VALIDUS_BUILD)_$(ANDROID_VERSION)_$(shell date -u +%Y%m%d-%H%M).$(VALIDUS_VERSION)-$(VALIDUS_BUILD_TYPE)
+VALIDUS_MOD_VERSION := VALIDUS_$(VALIDUS_BUILD)_$(ANDROID_VERSION)_$(shell date -u +%Y%m%d-%H%M).$(VALIDUS_VERSION)-$(VALIDUS_BUILD_TYPE)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \
-    ro.du.version=$(DU_VERSION) \
-    ro.mod.version=$(DU_BUILD_TYPE)-v11.0
+    ro.validus.version=$(VALIDUS_VERSION) \
+    ro.mod.version=$(VALIDUS_BUILD_TYPE)-v10.0
 
